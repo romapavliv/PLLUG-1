9c0da1e65c96e70ae7eac650c1df71d78a7247ae
NesterenkoYevhen <enesterenko76@gmail.com>
2021-09-24 20:36:57 +0300

    Add function for all words

--- hw_2.js
+++ hw_2.js
@@ -4,7 +4,7 @@ function crashBrowser() {
   }
 }
 
-function transformingString(word) {
+function transformingStringEnglish(word) {
   let upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   let newWord = '';
   for(let char of word) {
@@ -16,4 +16,18 @@ function transformingString(word) {
   }
   return newWord
 }
+
+function transformingStringAll(word) {
+  let wordUpperCase = word.toUpperCase();
+  let newWord = '';
+  for(let char of word) {
+    if(wordUpperCase.indexOf(char) != -1) {
+      newWord += char.toLowerCase();
+    } else {
+      newWord += char.toUpperCase()
+    }
+  }
+  return newWord
+}
+
 
